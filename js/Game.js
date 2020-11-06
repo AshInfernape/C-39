a53208ce403d96372a222408b2a23edaa0ff89a4
Divit <tekirukichi@gmail.com>
2020-11-06 14:11:06 +0530

    Made the car racing Stage 1.5

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1Image);
     car2 = createSprite(300,200);
+    car2.addImage(car2Image);
     car3 = createSprite(500,200);
+    car3.addImage(car3Image);
     car4 = createSprite(700,200);
+    car4.addImage(car4Image);
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("#484848")
+      image(trackImage,0,-displayHeight*4,displayWidth,displayHeight*5)
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 160;
       var y;
 
       for(var plr in allPlayers){
@@ -78,7 +84,12 @@ class Game {
       player.distance +=10
       player.update();
     }
-
+   if(player.distance > 3650){
+     gameState = 2
+   }
     drawSprites();
+  }
+  end(){
+    console.log("gameEnded")
   }
 }
